6de53216790290671edf07d7d598cd601a5164b7
Lucian Mihalca <infolucianmihalca@gmail.com>
2026-02-20 18:17:06 +0100

    chore: add minimal ESLint configuration and run lint in CI

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/consistent-type-imports': ['error'],
+    },
+  },
+]
